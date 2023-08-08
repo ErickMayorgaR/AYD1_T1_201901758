f1714633bb10748edb8f001387d2a96f5a4b277c
ErickMayorgaR <66332670+ErickMayorgaR@users.noreply.github.com>
2023-08-07 20:54:48 -0600

    Agregando Funcionalidad Calculadora

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+
+app.post('calculadora/201901758', (req, res) => {
+    const { numero1, numero2, operacion } = req.body;
+    if (typeof numero1 !== 'number' || typeof numero2 !== 'number') {
+        return res.status(400).json({ error: 'Los números Debe ingresar un valor numerico.' });
+      }
+    
+      let resultado;
+      switch (operacion) {
+        case 'suma':
+          resultado = numero1 + numero2;
+          break;
+        case 'resta':
+          resultado = numero1 - numero2;
+          break;
+        case 'multiplicacion':
+          resultado = numero1 + numero2;
+          break;
+        case 'division':
+          if (numero2 === 0) {
+            return res.status(400).json({ error: 'No se puede dividir por cero.' });
+          }
+          resultado = numero1 / numero2;
+          break;
+        default:
+          return res.status(400).json({ error: 'Operación no válida. Las opciones son: suma, resta, multiplicacion, division.' });
+      }
+    
+      res.json({ resultado });
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor en el puerto ${PORT}`);
+  });
